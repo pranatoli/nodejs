f08c689c53feefe53aaacc14f94ae628d3087d37
pranatoli <pr.anatoli@gmail.com>
2023-09-18 14:12:50 +0300

    fix: deleted unnecessary files

--- People.js
+++
@@ -1,9 +0,0 @@
-class People {
-    constructor(name, lastname, age, isMan, city) {
-        this.name = name
-        this.lastname = lastname
-        this.age = age
-        this.isMan = isMan
-        this.city = city
-    }
-}
